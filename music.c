d21cf9ea6a16c1ecdeb367c35bca554a8cd598db
James Reed <supplantr@archlinux.info>
2014-05-10 19:06:58 -0400

    Convert spaces to tabs

--- music.c
+++ music.c
@@ -15,10 +15,10 @@
 #define MPD_TIMEOUT 2
 
 struct mpd_data {
-    char album[MPD_BUF_LEN];
-    char artist[MPD_BUF_LEN];
-    char title[MPD_BUF_LEN];
-    MpdState state;
+	char album[MPD_BUF_LEN];
+	char artist[MPD_BUF_LEN];
+	char title[MPD_BUF_LEN];
+	MpdState state;
 };
 
 int mpd_conn = 0;
@@ -27,121 +27,121 @@ char *passwd = MPD_PASSWD;
 
 void set_mpd_data(MpdObj *mpd, ChangedStatusType st, struct mpd_data *data)
 {
-    if (st&MPD_CST_STATE || st&MPD_CST_SONGID) {
+	if (st&MPD_CST_STATE || st&MPD_CST_SONGID) {
 
-        mpd_Song *song = mpd_playlist_get_current_song(mpd);
-        data->state = mpd_player_get_state(mpd);
+		mpd_Song *song = mpd_playlist_get_current_song(mpd);
+		data->state = mpd_player_get_state(mpd);
 
-        if (song) {
-            if (song->album)
-                mpd_song_markup(data->album, MPD_BUF_LEN, "%album%", song);
-            else
-                strcpy(data->album, "");
-            if (song->artist)
-                mpd_song_markup(data->artist, MPD_BUF_LEN, "%artist%", song);
-            else
-                strcpy(data->artist, "");
-            if (song->title) {
-                mpd_song_markup(data->title, MPD_BUF_LEN, "%title%", song);
-            } else if (song->name) {
-                mpd_song_markup(data->title, MPD_BUF_LEN, "%name%", song);
-            } else if (song->file) {
-                mpd_song_markup(data->title, MPD_BUF_LEN, "%file%", song);
-            } else
-                strcpy(data->title, "");
-        }
-    }
+		if (song) {
+			if (song->album)
+				mpd_song_markup(data->album, MPD_BUF_LEN, "%album%", song);
+			else
+				strcpy(data->album, "");
+			if (song->artist)
+				mpd_song_markup(data->artist, MPD_BUF_LEN, "%artist%", song);
+			else
+				strcpy(data->artist, "");
+			if (song->title) {
+				mpd_song_markup(data->title, MPD_BUF_LEN, "%title%", song);
+			} else if (song->name) {
+				mpd_song_markup(data->title, MPD_BUF_LEN, "%name%", song);
+			} else if (song->file) {
+				mpd_song_markup(data->title, MPD_BUF_LEN, "%file%", song);
+			} else
+				strcpy(data->title, "");
+		}
+	}
 }
 
 void mpd_conn_changed(MpdObj *mpd, int connect, int *mpd_conn)
 {
-    if (!connect) {
-        mpd_disconnect(mpd);
-    }
-    *mpd_conn = connect;
+	if (!connect) {
+		mpd_disconnect(mpd);
+	}
+	*mpd_conn = connect;
 }
 
 int put_info(MpdObj *mpd, struct mpd_data *data)
 {
-    if (!mpd_conn) {
-        freopen("/dev/null", "w", stderr);
-        if (passwd)
-            mpd_send_password(mpd);
-        mpd_connect(mpd);
-    }
+	if (!mpd_conn) {
+		freopen("/dev/null", "w", stderr);
+		if (passwd)
+			mpd_send_password(mpd);
+		mpd_connect(mpd);
+	}
 
-    mpd_status_update(mpd);
+	mpd_status_update(mpd);
 
-    if (!mpd_conn) {
-        printf(format, "Not connected", "", "", "");
-        printf("\n");
-        fflush(stdout);
-        return EXIT_FAILURE;
-    }
+	if (!mpd_conn) {
+		printf(format, "Not connected", "", "", "");
+		printf("\n");
+		fflush(stdout);
+		return EXIT_FAILURE;
+	}
 
-    if (data->state == MPD_PLAYER_PAUSE) {
-        printf(format, "Paused", data->title, data->artist, data->album);
-    } else if (data->state == MPD_PLAYER_PLAY) {
-        printf(format, "Playing", data->title, data->artist, data->album);
-    } else if (data->state == MPD_PLAYER_STOP) {
-        printf(format, "Stopped", "", "", "");
-    }
+	if (data->state == MPD_PLAYER_PAUSE) {
+		printf(format, "Paused", data->title, data->artist, data->album);
+	} else if (data->state == MPD_PLAYER_PLAY) {
+		printf(format, "Playing", data->title, data->artist, data->album);
+	} else if (data->state == MPD_PLAYER_STOP) {
+		printf(format, "Stopped", "", "", "");
+	}
 
-    printf("\n");
-    fflush(stdout);
-    return EXIT_SUCCESS;
+	printf("\n");
+	fflush(stdout);
+	return EXIT_SUCCESS;
 }
 
 int main(int argc, char *argv[])
 {
-    bool snoop = false;
-    int interval = INTERVAL;
-    char *host = MPD_HOST;
-    int port = MPD_PORT;
-    int timeout = MPD_TIMEOUT;
+	bool snoop = false;
+	int interval = INTERVAL;
+	char *host = MPD_HOST;
+	int port = MPD_PORT;
+	int timeout = MPD_TIMEOUT;
 
-    char opt;
-    while ((opt = getopt(argc, argv, "hsf:i:o:p:w:t:")) != -1) {
-        switch (opt) {
-            case 'h':
-                printf("music [-h|-s|-f FORMAT|-i INTERVAL|-o HOST|-p PORT|-w PASSWORD|-t TIMEOUT]\n");
-                exit(EXIT_SUCCESS);
-                break;
-            case 's':
-                snoop = true;
-                break;
-            case 'f':
-                format = optarg;
-                break;
-            case 'i':
-                interval = atoi(optarg);
-                break;
-            case 'o':
-                host = optarg;
-                break;
-            case 'p':
-                port = atoi(optarg);
-                break;
-            case 'w':
-                passwd = optarg;
-                break;
-            case 't':
-                timeout = atoi(optarg);
-                break;
-        }
-    }
+	char opt;
+	while ((opt = getopt(argc, argv, "hsf:i:o:p:w:t:")) != -1) {
+		switch (opt) {
+			case 'h':
+				printf("music [-h|-s|-f FORMAT|-i INTERVAL|-o HOST|-p PORT|-w PASSWORD|-t TIMEOUT]\n");
+				exit(EXIT_SUCCESS);
+				break;
+			case 's':
+				snoop = true;
+				break;
+			case 'f':
+				format = optarg;
+				break;
+			case 'i':
+				interval = atoi(optarg);
+				break;
+			case 'o':
+				host = optarg;
+				break;
+			case 'p':
+				port = atoi(optarg);
+				break;
+			case 'w':
+				passwd = optarg;
+				break;
+			case 't':
+				timeout = atoi(optarg);
+				break;
+		}
+	}
 
-    MpdObj *mpd = mpd_new(host, port, passwd);
-    struct mpd_data data;
-    mpd_signal_connect_status_changed(mpd, (StatusChangedCallback)set_mpd_data, &data);
-    mpd_signal_connect_connection_changed(mpd, (ConnectionChangedCallback)mpd_conn_changed, &mpd_conn);
-    mpd_set_connection_timeout(mpd, timeout);
+	MpdObj *mpd = mpd_new(host, port, passwd);
+	struct mpd_data data;
+	mpd_signal_connect_status_changed(mpd, (StatusChangedCallback)set_mpd_data, &data);
+	mpd_signal_connect_connection_changed(mpd, (ConnectionChangedCallback)mpd_conn_changed, &mpd_conn);
+	mpd_set_connection_timeout(mpd, timeout);
 
-    if (snoop)
-        while (1) {
-            put_info(mpd, &data);
-            sleep(interval);
-        }
-    else
-        return put_info(mpd, &data);
+	if (snoop)
+		while (1) {
+			put_info(mpd, &data);
+			sleep(interval);
+		}
+	else
+		return put_info(mpd, &data);
 }
--- online.c
+++ online.c
@@ -13,64 +13,64 @@ char *interface = INTERFACE;
 
 int put_info(void)
 {
-    FILE *fp;
-    char path[64], state[4];
+	FILE *fp;
+	char path[64], state[4];
 
-    snprintf(path, sizeof(path), "/sys/class/net/%s/operstate", interface);
+	snprintf(path, sizeof(path), "/sys/class/net/%s/operstate", interface);
 
-    if ((fp = fopen(path, "r")) == NULL) {
-        fprintf(stderr, "error: can't open %s\n", path);
-        exit(EXIT_FAILURE);
-    }
+	if ((fp = fopen(path, "r")) == NULL) {
+		fprintf(stderr, "error: can't open %s\n", path);
+		exit(EXIT_FAILURE);
+	}
 
-    fscanf(fp, "%s", state);
-    fclose(fp);
+	fscanf(fp, "%s", state);
+	fclose(fp);
 
-    if (strncmp(state, "up", 2) == 0) {
-        printf(format, "Online");
-        printf("\n");
-        fflush(stdout);
-        return EXIT_SUCCESS;
-    }
+	if (strncmp(state, "up", 2) == 0) {
+		printf(format, "Online");
+		printf("\n");
+		fflush(stdout);
+		return EXIT_SUCCESS;
+	}
 
-    printf(format, "Offline");
-    printf("\n");
-    fflush(stdout);
-    return EXIT_FAILURE;
+	printf(format, "Offline");
+	printf("\n");
+	fflush(stdout);
+	return EXIT_FAILURE;
 }
 
 int main(int argc, char *argv[])
 {
-    bool snoop = false;
-    int interval = INTERVAL;
+	bool snoop = false;
+	int interval = INTERVAL;
 
-    char opt;
-    while ((opt = getopt(argc, argv, "hsf:i:w:")) != -1) {
-        switch (opt) {
-            case 'h':
-                printf("online [-h|-s|-f FORMAT|-i INTERVAL|-w INTERFACE]\n");
-                exit(EXIT_SUCCESS);
-                break;
-            case 's':
-                snoop = true;
-                break;
-            case 'f':
-                format = optarg;
-                break;
-            case 'i':
-                interval = atoi(optarg);
-                break;
-            case 'w':
-                interface = optarg;
-                break;
-        }
-    }
+	char opt;
+	while ((opt = getopt(argc, argv, "hsf:i:w:")) != -1) {
+		switch (opt) {
+			case 'h':
+				printf("online [-h|-s|-f FORMAT|-i INTERVAL|-w INTERFACE]\n");
+				exit(EXIT_SUCCESS);
+				break;
+			case 's':
+				snoop = true;
+				break;
+			case 'f':
+				format = optarg;
+				break;
+			case 'i':
+				interval = atoi(optarg);
+				break;
+			case 'w':
+				interface = optarg;
+				break;
+		}
+	}
 
-    if (snoop)
-        while (1) {
-            put_info();
-            sleep(interval);
-        }
-    else
-        return put_info();
+	if (snoop)
+		while (1) {
+			put_info();
+			sleep(interval);
+		}
+	else
+		return put_info();
 }
